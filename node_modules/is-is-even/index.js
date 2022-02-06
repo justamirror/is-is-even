768fdd1e9f169427b6e053f084a7cc2062432b64
Mirror <66569648+justamirror@users.noreply.github.com>
2022-02-06 19:36:56 +0000

    Initial commit

---
+++ node_modules/is-is-even/index.js
@@ -0,0 +1,5 @@
+const is_even = require("is-even");
+
+module.exports = function is_is_even(func) {
+	return func == is_even;
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+const is_is_even = require('./index.js')
+const is_even = require('is-even')
+if (is_is_even(is_even)) console.log("Test success")
+else console.log("Test Failure")
